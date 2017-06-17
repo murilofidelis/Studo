ec9ec4ee5315d4fa3247a7518807d7cb8d4d2388
Murilo Fidelis <murilofidelis2012@gmail.com>
2017-06-17 17:47:22 -0300

    alteração: config jdbc

--- src/main/java/br/com/studo/util/Conexao.java
+++ src/main/java/br/com/studo/util/Conexao.java
@@ -11,9 +11,14 @@ public class Conexao {
 	// BD_teste = studo_teste
 	// BD produção = STUDO
 
-	private static final String URL_CONEXAO = "jdbc:postgresql://localhost:5432/STUDO";
+	/*private static final String URL_CONEXAO = "jdbc:postgresql://localhost:5432/STUDO";
 	private static final String USUARIO = "postgres";
-	private static final String SENHA = "123456";
+	private static final String SENHA = "123456";*/
+	
+	
+	private static final String URL_CONEXAO = "jdbc:postgres://uhfnnqaklyktqt:f4199fc1f00b17cee937d0169efe1670182ba1b626e4e42994d7b2281af6271e@ec2-54-197-232-155.compute-1.amazonaws.com:5432/d61lt994e6cods";
+	private static final String USUARIO = "uhfnnqaklyktqt";
+	private static final String SENHA = "f4199fc1f00b17cee937d0169efe1670182ba1b626e4e42994d7b2281af6271e";
 
 	public static Connection getConexao() throws ClassNotFoundException, SQLException {
 
